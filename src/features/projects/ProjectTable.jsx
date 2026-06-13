e336572373357e5337efad12a3281d6681f577d7
pouya <pouyakhaleghi82@gmail.com>
2026-06-14 02:48:47 +0330

    feat:table for show projects details

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,19 +1,46 @@
 import useOwnerProjects from "./useOwnerProjects.js";
 import Loading from "../../ui/Loading.jsx";
+import Empty from "../../ui/Empty.jsx";
 
 function ProjectTable() {
-    const {projects , isPending} = useOwnerProjects()
+    const {projects , isPending} =  useOwnerProjects()
 
-    console.log(projects)
-    console.log(isPending)
+    if (isPending) return <Loading />
+    if (projects.length < 1) return <Empty resourceName={'پرژه'}/>
     return (
-        isPending?<Loading/>:<div>
-
-            hello
-
+        <div className="project-table">
+            <table>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>عنوان پرژه</th>
+                    <th>دسته بندی</th>
+                    <th>بودجه</th>
+                    <th>ددلاین</th>
+                    <th>تگ ها</th>
+                    <th>فریلنسر</th>
+                    <th>وضعیت</th>
+                    <th>عملیات</th>
+                </tr>
+                </thead>
+                <tbody className={''}>
+                {projects.map((project , index) => (
+                    <tr key={project.id}>
+                        <td>{index+1}</td>
+                        <td>{project.title}</td>
+                        <td>{project.category.title}</td>
+                        <td>{project.budget}</td>
+                        <td>{project.deadline}</td>
+                        <td><div className={'flex flex-wrap max-w-[200px] gap-x-4 gap-y-1 '}>{project.tags.map((tag, index)=>(<span className={'budge--secondary'} key={index}>{tag}</span>))}</div></td>
+                        <td>{project.freelancer?.name}</td>
+                        <td>{project.status==='OPEN'?'باز': 'بسته'}</td>
+                        <td>....</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
-
-    );
+    )
 }
 
 export default ProjectTable;
