49f91a491fadc7f0ae29462dc475945c0415461d
pouya <pouyakhaleghi82@gmail.com>
2026-06-18 02:28:20 +0330

    style:modal minimum width

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -6,7 +6,7 @@ function Modal({open, onClose , title, children}) {
 
     if (open) return (
         <div className={'fixed top-0 right-0 flex justify-center items-center w-screen h-screen bg-secondary-800/40 backdrop-blur-sm'}>
-            <div ref={ref} className={'bg-secondary-0 rounded-lg shadow-lg shadow-secondary-600 p-2 transition-all duration-500 ease-out max-w-9/12 z-10'}>
+            <div ref={ref} className={'bg-secondary-0 rounded-lg shadow-lg shadow-secondary-600 p-2 transition-all duration-500 ease-out max-w-9/12 min-w-50 z-10'}>
                 <div className={'flex justify-between items-center border-b border-secondary-200 p-2'}><h3>{title}</h3><button  onClick={onClose}><HiOutlineX/></button></div>
                 <div className={'p-2 overflow-hidden text-wrap'}>{children}</div>
             </div>
