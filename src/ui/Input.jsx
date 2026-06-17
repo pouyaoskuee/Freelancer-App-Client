1553a02489368704396e7a82add0fa375e384c42
pouya <pouyakhaleghi82@gmail.com>
2026-06-18 02:29:04 +0330

    feat:react-hook-form add to the project

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,20 +1,30 @@
 
-function Input({label , id , type , value , onChange , placeholder=''}) {
+function Input({label , id , type , placeholder='' , register ,validationSchema , errors ,required}) {
     return (
         <div>
-            <label
-                htmlFor={id}
-                className={'font-light'}
-            >{label}</label>
+            <div className={'flex items-center gap-1'}>
+                <label
+                    htmlFor={id}
+                    className={'font-normal'}
+                >{label}</label>
+                <p className={'text-error text-center h-4'}>{required && '*'}</p>
+            </div>
+
             <input
-                dir={'ltr'}
+                dir={type==='number'? 'ltr' : 'rtl'}
                 id={id}
                 type={type}
-                value={value}
-                onChange={e => onChange(e)}
-                className={'w-full font-thin  border border-secondary-200 bg-secondary-0  rounded-2xl p-2 hover:border-primary-500 focus:outline-none focus:bg-white focus:border-primary-500 focus:shadow-lg focus:shadow-primary-200 '}
                 placeholder={placeholder}
+                {...register(id , validationSchema)}
+                className={'w-full mt-2 font-thin  border border-secondary-200 bg-secondary-0  rounded-2xl p-2 hover:border-primary-500 focus:outline-none focus:bg-white focus:border-primary-500 focus:shadow-lg focus:shadow-primary-200 '}
             />
+            {
+                errors && errors[id] && (
+                    <span  className={'text-error text-sm font-thin'}>
+                        {errors[id]?.message}
+                    </span>
+                )
+            }
         </div>
     );
 }
