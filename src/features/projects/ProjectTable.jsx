67c2da745a7dc593cd83953a3180872c23bf262e
pouya <pouyakhaleghi82@gmail.com>
2026-06-21 04:36:03 +0330

    fix:Opening multiple medals

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -7,19 +7,14 @@ import {toPersianNumbersWithComma} from "../../utils/toPersianNumber.js";
 import {TbPencilMinus} from "react-icons/tb";
 import {HiOutlineTrash} from "react-icons/hi";
 import Modal from "../../ui/Modal.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
 import {useRemoveProject} from "./useRemoveProject.js";
 import CreateProject from "./CreateProject.jsx";
 
 function ProjectTable() {
     const {projects, isPending} = useOwnerProjects()
-    const [openEdit, setOpenEdit] = useState(false);
-    const [openRemove, setOpenRemove] = useState(false);
     const [openCreate, setOpenCreate] = useState(false);
-    const {isPending:removePending , mutate} = useRemoveProject()
-    const [projectId, setProjectId] = useState()
-
 
     if (isPending) return <Loading/>
     if (projects.length < 1) return <Empty resourceName={'پرژه'}/>
@@ -45,67 +40,7 @@ function ProjectTable() {
                 </tr>
                 </thead>
                 <tbody className={'bg-secondary-0 *:border-2 *:border-secondary-100'}>
-                {projects.map((project, index) => (
-                    <tr className={'*:px-4 *:py-2'} key={project._id}>
-                        <td>{index + 1}</td>
-                        <td>{truncateText(project.title, 30)}</td>
-                        <td>{project.category.title}</td>
-                        <td>{toPersianNumbersWithComma(project.budget)}</td>
-                        <td>{toLocalDateShort(project.deadline)}</td>
-                        <td>
-                            <div
-                                className={'flex flex-wrap max-w-[200px] gap-x-4 gap-y-1 '}>
-                                {project.tags.map((tag, index) => (
-                                <span className={'budge budge--secondary'} key={index}>{tag}</span>))}
-                            </div>
-                        </td>
-                        <td>{project.freelancer?.name}</td>
-                        <td>{project.status === 'OPEN' ? <p className={' budge budge--success text-center'}>باز</p> :
-                            <p className={' budge budge--error text-center'}>بسته</p>}</td>
-                        <td className={' space-x-2 text-nowrap'}>
-                            <button
-                                onClick={() => setOpenEdit(true)}
-                            ><TbPencilMinus
-                                className={'size-5 text-primary-900'}
-                            />
-                            </button>
-
-                            <button
-                                onClick={() => {
-                                    setOpenRemove(true)
-                                    setProjectId(project._id)
-                                } }
-                            ><HiOutlineTrash
-                                className={'size-5 text-error'}
-                            />
-                            </button>
-                            <div className={''}>
-                                <Modal
-                                    open={openEdit}
-                                    title={`ویرایش پرژه: ${project.title}`}
-                                    onClose={() => setOpenEdit(false)}
-                                > edite project edite project edite project edite projectedite project edite project edite project edite projectedite project edite project edite project edite projectedite project edite project edite project edite project
-                                </Modal>
-
-                                <Modal
-                                    open={openRemove}
-                                    title={`حذف پرژه: ${project.title}`}
-                                    onClose={() => setOpenRemove(false)}
-                                ><ConfirmDelete
-                                    disabled={removePending}
-                                    onConfirm={() => {
-                                        mutate(projectId)
-                                        setOpenRemove(false)
-                                    } }
-                                    resourceName={project.title}
-                                    onClose={() => setOpenRemove(false)}
-                                />
-                                </Modal>
-                            </div>
-                        </td>
-
-                    </tr>
-                ))}
+                {projects.map((project, index) => (<TableRow project={project} index={index} key={project._id}/>))}
                 </tbody>
             </table>
         </div>
@@ -113,3 +48,71 @@ function ProjectTable() {
 }
 
 export default ProjectTable;
+
+function TableRow({project , index}){
+    const [openEdit, setOpenEdit] = useState(false);
+    const [openRemove, setOpenRemove] = useState(false);
+    const {isPending:removePending , mutate} = useRemoveProject()
+
+
+    return (
+        <tr className={'*:px-4 *:py-2'} key={project._id}>
+            <td>{index + 1}</td>
+            <td>{truncateText(project.title, 30)}</td>
+            <td>{project.category.title}</td>
+            <td>{toPersianNumbersWithComma(project.budget)}</td>
+            <td>{toLocalDateShort(project.deadline)}</td>
+            <td>
+                <div
+                    className={'flex flex-wrap max-w-[200px] gap-x-4 gap-y-1 '}>
+                    {project.tags.map((tag, index) => (
+                        <span className={'budge budge--secondary'} key={index}>{tag}</span>))}
+                </div>
+            </td>
+            <td>{project.freelancer?.name}</td>
+            <td>{project.status === 'OPEN' ? <p className={' budge budge--success text-center'}>باز</p> :
+                <p className={' budge budge--error text-center'}>بسته</p>}</td>
+            <td className={' space-x-2 text-nowrap'}>
+                <button
+                    onClick={() => setOpenEdit(true)}
+                ><TbPencilMinus
+                    className={'size-5 text-primary-900'}
+                />
+                </button>
+
+                <button
+                    onClick={() => {
+                        setOpenRemove(true)
+                    } }
+                ><HiOutlineTrash
+                    className={'size-5 text-error'}
+                />
+                </button>
+                <div className={''}>
+                    <Modal
+                        open={openEdit}
+                        title={`ویرایش پرژه: ${project.title}`}
+                        onClose={() => setOpenEdit(false)}
+                    >
+                        <CreateProject project={project} onClose={()=>setOpenEdit(false)}/>
+                    </Modal>
+
+                    <Modal
+                        open={openRemove}
+                        title={`حذف پرژه: ${project.title}`}
+                        onClose={() => setOpenRemove(false)}
+                    ><ConfirmDelete
+                        disabled={removePending}
+                        onConfirm={() => {
+                            mutate(project._id)
+                            setOpenRemove(false)
+                        } }
+                        resourceName={project.title}
+                        onClose={() => setOpenRemove(false)}
+                    />
+                    </Modal>
+                </div>
+            </td>
+        </tr>
+    )
+}
