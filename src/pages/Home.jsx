9e87e2dd50ec5e939fb159ae530ee9355a13aadc
pouya <pouyakhaleghi82@gmail.com>
2026-07-02 03:01:29 +0330

    test:deploy test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom";
 
 function Home() {
     return (
         <div>
             its home page
+            <Link to={'/auth'}>go to auth</Link>
         </div>
     );
 }
