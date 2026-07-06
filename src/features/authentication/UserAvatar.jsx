bc8f09e49807e1b0791d9b09e86bf9ca9b45493a
pouya <pouyakhaleghi82@gmail.com>
2026-07-07 02:27:48 +0330

    style:Transfer header to the entire app

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,22 +1,28 @@
 import useUser from "./useUser.js";
 import Loading from "../../ui/Loading.jsx";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import NavigateList from "../../ui/NavigateList.jsx";
 
 function UserAvatar() {
     const {isLoading , user} =useUser()
     const navigate = useNavigate();
+    const [modal, setModal] = useState(false)
 
     if(isLoading) return <Loading/>
 
-    if (!user) return <div onClick={()=> navigate('/auth')}>ورود/ثبت نام</div>;
+    if (!user) return <div className={'cursor-pointer'} onClick={()=> navigate('/auth')}>ورود/ثبت نام</div>;
 
     return (
-        <div className={'flex items-center justify-center gap-2'}>
+        <div onClick={()=>setModal((prev)=>!prev)} className={'flex items-center justify-center gap-2 relative cursor-pointer '}>
             <img
                 className={' size-8 rounded-full'}
                 src="/user.jpg"
                 alt="userAvatar"/>
-            <span>{user?.name}</span>
+            <span className={'hidden sm:block '}>{user?.name}</span>
+            <div className={`bg-secondary-100 absolute top-11 -right-20 sm:right-0 opacity-0  max-h-0 rounded-md  transition-all duration-300 ${modal && 'opacity-100 max-h-dvh w-[12rem]' }  `}>
+                <NavigateList/>
+            </div>
         </div>
     );
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,15 +2,17 @@ import useUser from "../features/authentication/useUser.js";
 import HeaderMenu from "./HeaderMenu.jsx";
 import UserAvatar from "../features/authentication/UserAvatar.jsx";
 import NavBarMenu from "./NavBarMenu.jsx";
+import {useNavigate} from "react-router-dom";
 
 function Header() {
     const {isLoading} =useUser()
+    const navigate = useNavigate();
 
     return (
         <header className={` bg-secondary-0 text-secondary-900  px-2 border-secondary-100 flex justify-between items-center gap-2 sm:border-r-2 ${isLoading ? 'blur-sm opacity-50' : ''}`}>
             <div className={'flex items-center gap-3 sm:flex-row-reverse'}>
                 <NavBarMenu/>
-                <img src={'/logo/Deadlancer.png'} alt={'logo'} className={'size-20'}/>
+                <img onClick={()=>navigate('/')} src={'/logo/Deadlancer.png'} alt={'logo'} className={'size-20 cursor-pointer'} />
             </div>
             <div className={'flex items-center justify-center gap-4'}>
                 <UserAvatar />
