8217eaf5e241bb5654172c1aa35ea129b8fce332
pouya <pouyakhaleghi82@gmail.com>
2026-07-08 02:04:01 +0330

    style:dynamic sideBar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,29 @@
 import {Outlet} from "react-router-dom";
 import NavigateList from "./NavigateList.jsx";
 import {useEffect, useState} from "react";
+import {HiOutlineChevronLeft} from "react-icons/hi";
 
 function AppLayout() {
-    const [sideBar, setSideBar] = useState(true)
+    const [sideBar, setSideBar] = useState(false)
 
     useEffect(() => {
         setTimeout(()=>{
+            setSideBar(true)
+        },500)
+        setTimeout(()=>{
             setSideBar(false)
-        },1000)
+        },2000)
     },[])
 
     return (
-        <div className={` bg-secondary-100 grid grid-cols-[0rem_minmax(10rem,1fr)] transition-all duration-1000 *:p-0 *:transition-all *:duration-1000 ${sideBar && 'grid-cols-[10rem_minmax(10rem,1fr)]! *:p-4 ' } grid-rows-[5rem_1fr] gap-15 `}>
-            <aside className={' overflow-auto bg-secondary-0 row-span-full h-screen relative '} >
+        <div className={` grid transition-all duration-1000 ease-out ${sideBar ? ' grid-cols-[7rem_minmax(10rem,1fr)]  sm:grid-cols-[13rem_minmax(10rem,1fr)]':'grid-cols-[0rem_minmax(10rem,1fr)]' } grid-rows-[5rem_1fr] `}>
+            <aside className={` *:transition-all *:duration-1000 *:ease-out overflow-auto bg-secondary-0 row-span-full h-screen  ${sideBar ? 'p-2 sm:p-5':'p-0'} `} >
                 <NavigateList/>
-                <div onClick={()=>setSideBar((prevState)=>!prevState)} className={`fixed top-1/2 right-0 transition-all duration-1000 ${sideBar && 'right-35'} `}>icon</div>
+                <div onClick={()=>setSideBar((prevState)=>!prevState)} className={` size-8 flex items-center justify-center rounded-full bg-secondary-0 fixed top-1/2 -right-2 transition-all duration-1000 ${sideBar && ' right-19 rotate-180 sm:right-44'} `}>
+                    <HiOutlineChevronLeft size={'25'}/>
+                </div>
             </aside>
-
-
-            <div className={'h-full'}>
+            <div className={' bg-secondary-100 h-dvh overflow-auto p-2 sm:p-6'}>
                 <Outlet/>
             </div>
         </div>
